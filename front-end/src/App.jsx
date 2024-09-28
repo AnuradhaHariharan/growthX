0609e02f250644f6ddfc7685526a31b9a6926e23
Anuradha Hariharan <anuradhahariharan@192.168.1.12>
2024-09-28 23:36:12 +0530

    front-end fixl

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
+import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Placeorder from './pages/Placeorder/Placeorder'
 import Verify from './pages/Verify/Verify'
